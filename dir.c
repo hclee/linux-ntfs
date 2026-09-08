e34547223f95442a83b46f1d03c7dc89cd9c6035
Hyunchul Lee <hyc.lee@gmail.com>
2026-09-08 13:58:54 +0900

    ntfs: fix lookup of POSIX names with Windows-invalid characters
    POSIX FILE_NAME entries may contain characters such as '?' that are
    invalid in the Windows namespace. Directory index lookup currently
    passes an invalid-character error value to ntfs_collate_names(),
    causing lookup and removal of such names to fail with -EINVAL.
    
    Allow callers to disable invalid-character errors when collation is
    used for index traversal, and suppress those errors when comparing
    POSIX FILE_NAME attributes.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- dir.c
+++ dir.c
@@ -244,7 +244,7 @@ found_it:
 		 */
 		rc = ntfs_collate_names(uname, uname_len,
 				(__le16 *)&ie->key.file_name.file_name,
-				ie->key.file_name.file_name_length, 1,
+				ie->key.file_name.file_name_length, false,
 				IGNORE_CASE, vol->upcase, vol->upcase_len);
 		/*
 		 * If uname collates before the name of the current entry, there
@@ -263,7 +263,7 @@ found_it:
 		 */
 		rc = ntfs_collate_names(uname, uname_len,
 				(__le16 *)&ie->key.file_name.file_name,
-				ie->key.file_name.file_name_length, 1,
+				ie->key.file_name.file_name_length, false,
 				CASE_SENSITIVE, vol->upcase, vol->upcase_len);
 		if (rc == -1)
 			break;
@@ -502,7 +502,7 @@ found_it2:
 		 */
 		rc = ntfs_collate_names(uname, uname_len,
 				(__le16 *)&ie->key.file_name.file_name,
-				ie->key.file_name.file_name_length, 1,
+				ie->key.file_name.file_name_length, false,
 				IGNORE_CASE, vol->upcase, vol->upcase_len);
 		/*
 		 * If uname collates before the name of the current entry, there
@@ -521,7 +521,7 @@ found_it2:
 		 */
 		rc = ntfs_collate_names(uname, uname_len,
 				(__le16 *)&ie->key.file_name.file_name,
-				ie->key.file_name.file_name_length, 1,
+				ie->key.file_name.file_name_length, false,
 				CASE_SENSITIVE, vol->upcase, vol->upcase_len);
 		if (rc == -1)
 			break;
